20309f162ae63ec88996002c92b383d89818446c
flagz_ <khoint828@proton.me>
2024-04-10 17:49:48 +0300

    Sửa bài tập thực hành số 17

--- btth17/script.js
+++ btth17/script.js
@@ -8,24 +8,27 @@ let active = 0;
 let lengthItems = items.length;
 
 next.onclick = function () {
-  if (active + 1 > lengthItems) {
+  if (active + 1 >= lengthItems) {
     active = 0;
   } else {
     active = active + 1;
   }
   reloadSlider();
 };
-prev.onclick = function () {
+
+priv.onclick = function () {
   if (active - 1 < 0) {
-    active = lengthItems;
+    active = lengthItems - 1;
   } else {
     active = active - 1;
   }
   reloadSlider();
 };
+
 let refreshSlider = setInterval(() => {
   next.click();
 }, 3000);
+
 function reloadSlider() {
   let checkLeft = items[active].offsetLeft;
   list.style.left = -checkLeft + "px";
@@ -33,11 +36,13 @@ function reloadSlider() {
   let lastActiveDot = document.querySelector(".slider .dots li.active");
   lastActiveDot.classList.remove("active");
   dots[active].classList.add("active");
+
   clearInterval(refreshSlider);
   refreshSlider = setInterval(() => {
     next.click();
   }, 3000);
 }
+
 dots.forEach((li, key) => {
   li.addEventListener("click", function () {
     active = key;
